d8cd9a4d24449bdbeb0d747080e98940daade01f
Vasilii Sotnikov <vsotnikov@icloud.com>
2025-06-02 23:48:48 +0300

    CI setup, cats either syntax rewrites

--- output/src/main/scala/fix/UncatYourProgram.scala
+++ output/src/main/scala/fix/UncatYourProgram.scala
@@ -2,6 +2,7 @@ package fix
 
 
 
+
 import zio.ZIO
 
 import scala.util.Try
@@ -15,5 +16,15 @@ object UncatYourProgram {
   def unignoreTest(): Unit = {
     (1 + 1): Unit
     ZIO.succeed(1).ignore: Unit
+  }
+
+  def uneitherTest(): Unit = {
+    val l = Left("no")
+    val r = Right("yes")
+    val x = Either.cond(false, 1, "no").left.map(_ + "!!1")
+    ZIO.succeed(1).asLeft: Unit
+    print(s"left: $l")
+    print(s"right: $r")
+    print(s"x is: $x")
   }
 }
--- rules/src/main/scala/fix/UncatYourProgram.scala
+++ rules/src/main/scala/fix/UncatYourProgram.scala
@@ -8,9 +8,17 @@ final class UncatYourProgram extends SemanticRule("UncatYourProgram") {
   override val description: String =
     "Semantic rule to rewrite TypeLevel libraries syntax to stdlib"
 
+  private val foldCatcher = SymbolMatcher.normalized("mouse.BooleanOps.fold")
+  private val ignoreCatcher =
+    SymbolMatcher.normalized("mouse.MouseFunctions.ignore")
+  private val eitherIdRightCatcher =
+    SymbolMatcher.normalized("cats/syntax.EitherIdOps#asRight")
+  private val eitherIdLeftCatcher =
+    SymbolMatcher.normalized("cats/syntax.EitherIdOps#asLeft")
+  private val eitherLeftMapCatcher =
+    SymbolMatcher.normalized("cats/syntax.EitherOps#leftMap")
+
   override def fix(implicit doc: SemanticDocument): Patch = {
-    val foldCatcher = SymbolMatcher.normalized("mouse.BooleanOps.fold")
-    val ignoreCatcher = SymbolMatcher.normalized("mouse.MouseFunctions.ignore")
     doc.tree.collect {
       case foldCatcher(
             x @ Term.Apply(
@@ -42,6 +50,31 @@ final class UncatYourProgram extends SemanticRule("UncatYourProgram") {
             ) :: Nil
           ) =>
         Patch.removeTokens(ignoreImport.tokens)
+      case eitherIdRightCatcher(
+            x @ Term.Select(extendedTerm, Term.Name("asRight"))
+          ) =>
+        Patch.replaceTree(x, s"Right(${extendedTerm.syntax})")
+      case eitherIdLeftCatcher(
+            x @ Term.Select(extendedTerm, Term.Name("asLeft"))
+          ) =>
+        Patch.replaceTree(x, s"Left(${extendedTerm.syntax})")
+      case eitherLeftMapCatcher(
+            x @ Term.Apply(
+              Term.Select(extendedTerm, Term.Name("leftMap")),
+              func :: Nil
+            )
+          ) =>
+        Patch.replaceTree(x, s"${extendedTerm.syntax}.left.map(${func.syntax})")
+      case catsEitherSyntaxImport @ Import(
+            Importer(
+              Term.Select(
+                Term.Select(Term.Name("cats"), Term.Name("syntax")),
+                Term.Name("either")
+              ),
+              _
+            ) :: Nil
+          ) =>
+        Patch.removeTokens(catsEitherSyntaxImport.tokens)
     }.asPatch
   }
 
